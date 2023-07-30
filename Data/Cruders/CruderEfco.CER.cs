793f0d459b2269e60f37ea4ec996f2f542f5fa1f
Daniel Stutz <admin@dstutz.ch>
2023-07-30 11:58:57 +0200

    Improved classes in namespace Coder

--- Data/Cruders/CruderEfco.CER.cs
+++ Data/Cruders/CruderEfco.CER.cs
@@ -1,6 +1,4 @@
-﻿using DStutz.Coder.Entities.Data;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
 
